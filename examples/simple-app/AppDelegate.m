137d087dab9854a992ac52dd6501e4b0faec040b
KOed <talentljl@gmail.com>
2012-04-22 11:32:04 +0800

    Make example fitting recent modifications.

--- examples/simple-app/AppDelegate.m
+++ examples/simple-app/AppDelegate.m
@@ -5,20 +5,17 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
   // Create a new browser & window when we start
-  CTBrowserWindowController* windowController =
-      [[CTBrowserWindowController alloc] initWithBrowser:[MyBrowser browser]];
-  [windowController.browser addBlankTabInForeground:YES];
-  [windowController showWindow:self];
-  // Because window controller are owned by the app, we need to release our
-  // reference.
-  //[windowController autorelease];
+	MyBrowser* browser = (MyBrowser *)[MyBrowser browser];
+	browser.windowController = [[CTBrowserWindowController alloc] initWithBrowser:browser];
+	[browser addBlankTabInForeground:YES];
+	[browser.windowController showWindow:self];
 }
 
 // When there are no windows in our application, this class (AppDelegate) will
 // become the first responder. We forward the command to the browser class.
 - (void)commandDispatch:(id)sender {
-  NSLog(@"commandDispatch %d", (int)[sender tag]);
-  [MyBrowser executeCommand:[sender tag]];
+	NSLog(@"commandDispatch %d", (int)[sender tag]);
+	[MyBrowser executeCommand:[sender tag]];
 }
 
 @end
